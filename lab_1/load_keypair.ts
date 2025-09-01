6970fc7fc5ce4700c1c40d50c94a4e48ad2c439b
Andrei <andreinicula64@gmail.com>
2025-09-01 23:00:04 +0300

    Small patches

--- lab_1/load_keypair.ts
+++ lab_1/load_keypair.ts
@@ -2,8 +2,10 @@ import "dotenv/config";
 import {
     getKeypairFromEnvironment
 } from "@solana-developers/helpers";
+import bs58 from "bs58";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
 console.log("Keypair loaded!");
 console.log("Public key: ", keypair.publicKey.toBase58());
+console.log("Private key: ", bs58.encode(keypair.secretKey));
