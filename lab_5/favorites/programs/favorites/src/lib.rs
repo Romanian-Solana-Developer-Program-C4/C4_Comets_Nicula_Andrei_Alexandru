506c45f668afbafc93286e984fc13ccb3836e2f2
Andrei <andreinicula64@gmail.com>
2025-10-01 17:56:22 +0300

    Favorites Final Commit

--- lab_5/favorites/programs/favorites/src/lib.rs
+++ lab_5/favorites/programs/favorites/src/lib.rs
@@ -2,52 +2,46 @@ use anchor_lang::prelude::*;
 
 declare_id!("UU7m8MqbZgqR7KwkfvQUyBVoZazHzh1An5rhjorJ5kG");
 pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod favorites {
     use super::*;
 
-    pub fn set_favorites(context: Context<SetFavorites>, number: i128, color: String, foods: Vec<String>) -> Result<()> {
-        let user_pk = context.accounts.user.key();
-
-        msg!("Program ID: {context.program_id}");
-        msg!("User {user_PK}'s favorite number is {number}, favorite color is {color}\
-              and favorite foods are: {:?}", foods);
-
+    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String, hobbies:
+    Vec<String>) -> Result<()> {
+        let user_public_key = context.accounts.user.key();
+        msg!("Greetings from {}", context.program_id);
+        msg!("User {user_public_key}'s favorite number is {number}, favorite color is: {color}");
+        msg!("User's hobbies are: {:?}", hobbies);
         context.accounts.favorites.set_inner(Favorites {
-            user: context.accounts.user.key(),
             number,
             color,
-            foods
+            hobbies
         });
-
         Ok(())
     }
 }
+
 #[derive(Accounts)]
 pub struct SetFavorites<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
-
     #[account(
-        init_if_needed,
-        payer = user,
-        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
-        seeds = [b"favorites", user.key().as_ref()],
-        bump,
-        has_one = user,
-    )]
+init,
+payer = user,
+space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+seeds=[b"favorites", user.key().as_ref()],
+bump)]
     pub favorites: Account<'info, Favorites>,
-
     pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
 pub struct Favorites {
-    pub user: Pubkey,
-    pub number: i128,
-    #[max_len(25)]
+    pub number: u64,
+    #[max_len(50)]
     pub color: String,
-    #[max_len(10, 50)]
-    pub foods: Vec<String>
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>
 }
--- lab_5/favorites/tests/favorites.ts
+++ lab_5/favorites/tests/favorites.ts
@@ -8,15 +8,15 @@ const web3 = anchor.web3;
 describe("Favorites", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
-  const program = anchor.workspace.Favorites as Program<Favorites>;
   const user = (provider.wallet as anchor.Wallet).payer;
+  const program = anchor.workspace.Favorites as Program<Favorites>;
 
   it("Saves a user's favorites to the blockchain", async () => {
-    const favoriteNumber = new anchor.BN(-5000);
-    const favoriteColor = "blue";
-    const favoriteFoods = ["burger", "pizza", "shaorma"];
+    const favoriteNumber = new anchor.BN(23);
+    const favoriteColor = "purple";
+    const favoriteHobbies = ["skiing", "skydiving", "biking"];
     await program.methods
-        .setFavorites(favoriteNumber, favoriteColor, favoriteFoods)
+        .setFavorites(favoriteNumber, favoriteColor, favoriteHobbies)
         .signers([user])
         .rpc();
     const favoritesPdaAndBump = web3.PublicKey.findProgramAddressSync(
@@ -27,18 +27,20 @@ describe("Favorites", () => {
     const dataFromPda = await program.account.favorites.fetch(favoritesPda);
     assert.equal(dataFromPda.color, favoriteColor);
     assert.equal(dataFromPda.number.toString(), favoriteNumber.toString());
-    assert.deepEqual(dataFromPda.foods, favoriteFoods);
+    assert.deepEqual(dataFromPda.hobbies, favoriteHobbies);
   });
+
   it("Doesn't let people write to favorites for other users", async () => {
     const someRandomGuy = anchor.web3.Keypair.generate();
     try {
       await program.methods
-          .setFavorites(new anchor.BN(420), "red", ["pasta", "salad"])
+          .setFavorites(new anchor.BN(420), "red", ["being a dork"])
           .signers([someRandomGuy])
           .rpc();
     } catch (error) {
       const errorMessage = (error as Error).message;
-      assert.isTrue(errorMessage.includes("Unknown Signer!"));
+      assert.isTrue(errorMessage.includes("unknown signer"));
     }
   });
+
 });
